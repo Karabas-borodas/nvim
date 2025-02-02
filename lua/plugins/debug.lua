7224c4069a1c3b2506112b0662f8df6efb47bea7
admin <anglefar>
2025-02-02 11:39:13 +0300

    DONE :some debuging

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -1,121 +1,75 @@
 return {
-	{
-		"mfussenegger/nvim-dap",
-		dependencies = {
-			"leoluz/nvim-dap-go",
-			"rcarriga/nvim-dap-ui",
-			"theHamsta/nvim-dap-virtual-text",
-			"nvim-neotest/nvim-nio",
-			"williamboman/mason.nvim",
-			"vadimcn/vscode-lldb",
-			"jay-babu/mason-nvim-dap.nvim",
-		},
-		config = function()
-			local dap = require("dap")
-			local ui = require("dapui")
+  {
+    'mfussenegger/nvim-dap',
+    dependencies = {
+      'leoluz/nvim-dap-go',
+      'rcarriga/nvim-dap-ui',
+      'theHamsta/nvim-dap-virtual-text',
+      'nvim-neotest/nvim-nio',
+      'williamboman/mason.nvim',
+      'vadimcn/vscode-lldb',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    config = function()
+      local dap = require 'dap'
+      local ui = require 'dapui'
 
-			-- Setup nvim-dap-ui
-			ui.setup()
+      require('dapui').setup()
+      require('dap-go').setup()
 
-			-- Setup nvim-dap-go
-			require("dap-go").setup()
+      require('mason-nvim-dap').setup {
+        automatic_setup = true,
+        handlers = {},
+        ensure_installed = {
+          'delve',
+        },
+      }
+      require('nvim-dap-virtual-text').setup {}
 
-			-- Setup mason-nvim-dap
-			require("mason-nvim-dap").setup({
-				-- Makes a best effort to setup the various debuggers with
-				-- reasonable debug configurations
-				automatic_setup = true,
+      local elixir_ls_debugger = vim.fn.exepath 'elixir-ls-debugger'
+      if elixir_ls_debugger ~= '' then
+        dap.adapters.mix_task = {
+          type = 'executable',
+          command = elixir_ls_debugger,
+        }
 
-				-- You can provide additional configuration to the handlers,
-				-- see mason-nvim-dap README for more information
-				handlers = {},
+        dap.configurations.elixir = {
+          {
+            type = 'mix_task',
+            name = 'phoenix server',
+            task = 'phx.server',
+            request = 'launch',
+            projectDir = '${workspaceFolder}',
+            exitAfterTaskReturns = false,
+            debugAutoInterpretAllModules = false,
+          },
+        }
+      end
 
-				-- You'll need to check that you have the required things installed
-				-- online, please don't ask me how to install them :)
-				ensure_installed = {
-					-- Update this to ensure that you have the debuggers for the langs you want
-					"delve",
-				},
-			})
+      vim.keymap.set('n', '<space>b', dap.toggle_breakpoint)
+      vim.keymap.set('n', '<space>gb', dap.run_to_cursor)
+      vim.keymap.set('n', '<space>?', function()
+        require('dapui').eval(nil, { enter = true })
+      end)
+      vim.keymap.set('n', '<F1>', dap.continue)
+      vim.keymap.set('n', '<F2>', dap.step_into)
+      vim.keymap.set('n', '<F3>', dap.step_over)
+      vim.keymap.set('n', '<F4>', dap.step_out)
+      vim.keymap.set('n', '<F5>', dap.step_back)
+      vim.keymap.set('n', '<F13>', dap.restart)
 
-			-- Setup nvim-dap-virtual-text
-			require("nvim-dap-virtual-text").setup({
-				-- NOTE: TJ DeVries: This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
-				-- display_callback = function(variable)
-				--   local name = string.lower(variable.name)
-				--   local value = string.lower(variable.value)
-				--   if name:match 'secret' or name:match 'api' or value:match 'secret' or value:match 'api' then
-				--     return '*****'
-				--   end
-				--
-				--   if #variable.value > 15 then
-				--     return ' ' .. string.sub(variable.value, 1, 15) .. '... '
-				--   end
-				--
-				--   return ' ' .. variable.value
-				-- end,
-			})
-
-			-- Handled by nvim-dap-go
-			-- dap.adapters.go = {
-			--   type = "server",
-			--   port = "${port}",
-			--   executable = {
-			--     command = "dlv",
-			--     args = { "dap", "-l", "127.0.0.1:${port}" },
-			--   },
-			-- }
-
-			-- Setup Elixir debugger if available
-			local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
-			if elixir_ls_debugger ~= "" then
-				dap.adapters.mix_task = {
-					type = "executable",
-					command = elixir_ls_debugger,
-				}
-
-				dap.configurations.elixir = {
-					{
-						type = "mix_task",
-						name = "phoenix server",
-						task = "phx.server",
-						request = "launch",
-						projectDir = "${workspaceFolder}",
-						exitAfterTaskReturns = false,
-						debugAutoInterpretAllModules = false,
-					},
-				}
-			end
-
-			-- Key mappings for nvim-dap
-			vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-			vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-
-			-- Evaluate variable under cursor
-			vim.keymap.set("n", "<space>?", function()
-				require("dapui").eval(nil, { enter = true })
-			end)
-
-			vim.keymap.set("n", "<F1>", dap.continue)
-			vim.keymap.set("n", "<F2>", dap.step_into)
-			vim.keymap.set("n", "<F3>", dap.step_over)
-			vim.keymap.set("n", "<F4>", dap.step_out)
-			vim.keymap.set("n", "<F5>", dap.step_back)
-			vim.keymap.set("n", "<F13>", dap.restart)
-
-			-- Listeners for nvim-dap-ui
-			dap.listeners.before.attach.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.launch.dapui_config = function()
-				ui.open()
-			end
-			dap.listeners.before.event_terminated.dapui_config = function()
-				ui.close()
-			end
-			dap.listeners.before.event_exited.dapui_config = function()
-				ui.close()
-			end
-		end,
-	},
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }
