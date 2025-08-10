f58f268921dbc854a39a13d6a57acd9f868b87eb
Mihail <zoood3.14@mail.com>
2025-08-10 11:39:21 +0300

    DEBUG: work but dont stop

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -6,215 +6,143 @@
 -- be extended to other languages as well.
 
 return {
-  -- NOTE: Yes, you can install new plugins here!
-  'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
-  keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
-    {
-      '<F5>',
-      function()
-        require('dap').continue()
-      end,
-      desc = 'Debug: Start/Continue',
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "leoluz/nvim-dap-go",
+      "rcarriga/nvim-dap-ui",
+      "theHamsta/nvim-dap-virtual-text",
+      "nvim-neotest/nvim-nio",
+      "williamboman/mason.nvim",
+      "vadimcn/vscode-lldb",
+      "jay-babu/mason-nvim-dap.nvim",
+      "tomblind/local-lua-debugger-vscode",
     },
-    {
-      '<F1>',
-      function()
-        require('dap').step_into()
-      end,
-      desc = 'Debug: Step Into',
-    },
-    {
-      '<F2>',
-      function()
-        require('dap').step_over()
-      end,
-      desc = 'Debug: Step Over',
-    },
-    {
-      '<F3>',
-      function()
-        require('dap').step_out()
-      end,
-      desc = 'Debug: Step Out',
-    },
-    {
-      '<leader>b',
-      function()
-        require('dap').toggle_breakpoint()
-      end,
-      desc = 'Debug: Toggle Breakpoint',
-    },
-    {
-      '<leader>B',
-      function()
-        require('dap').set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-      end,
-      desc = 'Debug: Set Breakpoint',
-    },
-    -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-    {
-      '<F7>',
-      function()
-        require('dapui').toggle()
-      end,
-      desc = 'Debug: See last session result.',
-    },
-  },
-  config = function()
-    local dap = require 'dap'
-    local dapui = require 'dapui'
+    config = function()
+      local dap = require("dap")
+      local ui = require("dapui")
 
-    require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
+      require("dapui").setup()
+      require("dap-go").setup()
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+      require("mason-nvim-dap").setup({
+        -- Makes a best effort to setup the various debuggers with
+        -- reasonable debug configurations
+        automatic_setup = true,
+        automatic_installation = true,
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
-        'cppdbg',
-      },
-    }
+        -- You can provide additional configuration to the handlers,
+        -- see mason-nvim-dap README for more information
+        handlers = {},
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
-    dapui.setup {
-      icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
-      mappings = {
-        -- Example mappings
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-        toggle = "t",
-      },
-      element_mappings = {
-        -- Example element mappings
-        stacks = {
-          open = "o",
+        -- You'll need to check that you have the required things installed
+        -- online, please don't ask me how to install them :)
+        ensure_installed = {
+          -- Update this to ensure that you have the debuggers for the langs you want
+          "delve",
+          "codelldb",
+          "local-lua-debugger-vscode",
         },
-        breakpoints = {
-          toggle = "t",
-        },
-      },
-      expand_lines = true,
-      force_buffers = true,
-      layouts = {
-        {
-          elements = {
-            { id = "scopes", size = 0.25 },
-            { id = "breakpoints", size = 0.25 },
-            { id = "stacks", size = 0.25 },
-            { id = "watches", size = 0.25 },
-          },
-          position = "left",
-          size = 40,
-        },
-        {
-          elements = {
-            { id = "repl", size = 0.5 },
-            { id = "console", size = 0.5 },
-          },
-          position = "bottom",
-          size = 10,
-        },
-      },
-      floating = {
-        max_height = nil,
-        max_width = nil,
-        border = "single",
-        mappings = {
-          close = { "q", "<Esc>" },
-        },
-      },
-      controls = {
-        enabled = true,
-        element = "repl",
-        icons = {
-     pause = '⏸',
-      play = '▶',
-      step_into = '⏎',
-      step_over = '⏭',
-      step_out = '⏮',
-      step_back = 'b',
-      run_last = '▶▶',
-      terminate = '⏹',
-      disconnect = '⏏',
+      })
+      require("nvim-dap-virtual-text").setup({
+        -- NOTE: TJ DeVries: This just tries to mitigate the chance that I leak tokens here. Probably won't stop it from happening...
+        --
+        display_callback = function(variable)
+          local name = string.lower(variable.name)
+          local value = string.lower(variable.value)
+          if name:match("secret") or name:match("api") or value:match("secret") or value:match("api") then
+            return "*****"
+          end
 
-  },      },
-      render = {
-        max_type_length = nil,
-        indent = 1,
-      },
-    }
+          if #variable.value > 15 then
+            return " " .. string.sub(variable.value, 1, 15) .. "... "
+          end
 
-    dap.listeners.after.event_initialized['dapui_config'] = dapui.open
-    dap.listeners.before.event_terminated['dapui_config'] = dapui.close
-    dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
-
-    -- C and C++ configuration
-    dap.adapters.cppdbg = {
-      id = 'cppdbg',
-      type = 'executable',
-      command = 'OpenDebugAD7',
-    }
-
-    dap.configurations.cpp = {
-      {
-        name = 'Launch file',
-        type = 'cppdbg',
-        request = 'launch',
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+          return " " .. variable.value
         end,
-        cwd = '${workspaceFolder}',
-        stopAtEntry = true,
-      },
-      {
-        name = 'Attach to process',
-        type = 'cppdbg',
-        request = 'attach',
-        processId = require('dap.utils').pick_process,
-        program = function()
-          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-        end,
-        cwd = '${workspaceFolder}',
-      },
-    }
+      })
 
-    dap.configurations.c = dap.configurations.cpp
-  end,
+      -- Handled by nvim-dap-go
+      -- dap.adapters.go = {
+      --   type = "server",
+      --   port = "${port}",
+      --   executable = {
+      --     command = "dlv",
+      --     args = { "dap", "-l", "127.0.0.1:${port}" },
+      --   },
+      -- }
+
+      -- Evgeny Koltsov: This is config for Lua from ChatGPT. Doesn't work yet.
+      -- -- Configure the Lua Debugger Adapter (local-lua-debugger-vscode)
+      -- dap.adapters.lua = {
+      --   type = "server",
+      --   host = "127.0.0.1", -- Debugger server will run on localhost
+      --   port = 8086, -- Default port for local-lua-debugger-vscode, change if necessary
+      --   executable = {
+      --     command = "~/.local/share/nvim/lazy/local-lua-debugger-vscode", -- Replace with the path to the debugger binary
+      --     args = { "--server" }, -- Arguments to run the debugger in server mode
+      --   },
+      -- }
+      --
+      -- -- Lua Debugger launch configuration
+      -- dap.configurations.lua = {
+      --   {
+      --     type = "lua",
+      --     request = "launch",
+      --     name = "Launch Lua file",
+      --     program = "${file}", -- Program to run (current file)
+      --   },
+      -- }
+
+      local elixir_ls_debugger = vim.fn.exepath("elixir-ls-debugger")
+      if elixir_ls_debugger ~= "" then
+        dap.adapters.mix_task = {
+          type = "executable",
+          command = elixir_ls_debugger,
+        }
+
+        dap.configurations.elixir = {
+          {
+            type = "mix_task",
+            name = "phoenix server",
+            task = "phx.server",
+            request = "launch",
+            projectDir = "${workspaceFolder}",
+            exitAfterTaskReturns = false,
+            debugAutoInterpretAllModules = false,
+          },
+        }
+      end
+
+      vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
+      vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
+
+      -- Eval var under cursor
+      vim.keymap.set("n", "<space>?", function()
+        require("dapui").eval(nil, { enter = true })
+      end)
+
+      vim.keymap.set("n", "<F1>", dap.continue)
+      vim.keymap.set("n", "<F2>", dap.step_into)
+      vim.keymap.set("n", "<F3>", dap.step_over)
+      vim.keymap.set("n", "<F4>", dap.step_out)
+      vim.keymap.set("n", "<F5>", dap.step_back)
+      vim.keymap.set("n", "<F6>", dap.restart)
+      vim.keymap.set("n", "<F8>", dap.terminate)
+      vim.keymap.set("n", "<F9>", dap.toggle_breakpoint)
+
+      dap.listeners.before.attach.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        ui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        ui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        ui.close()
+      end
+    end,
+  },
 }
-
