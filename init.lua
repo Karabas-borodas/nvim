056a3128aeb2eabca7a33ea579d618baea8c03d3
Mihail <zoood3.14@mail.com>
2025-11-15 11:07:26 +0300

    GO: nvim copy

--- init.lua
+++ init.lua
@@ -1,25 +1,6 @@
---Basic
-require("core.plugins")
-require("core.mappings")
-require("core.colors")
-
---Plugins
-require("plugins.neotree")
-require("plugins.treesitter")
-require("plugins.mason")
-require("plugins.some_plagins")
-require("plugins.lsp")
-require("plugins.telescope")
-require("plugins.cmp")
-
---require('plugins.nullls')
-require("plugins.debug")
-require("plugins.tmux_navigator")
-require("plugins.terminal")
-require("plugins.buffline")
-require("plugins.planery")
---require('plugins.lsp')
-vim.cmd([[
-  autocmd VimEnter * if filereadable("mix.exs") | silent !mix phx.server & | endif
-]])
-vim.cmd("autocmd VimEnter * !mix archive.install hex phx_new --force")
+vim.g.mapleader = " "
+vim.g.maplocalleader = ","
+_G.vnext = {}
+require("vnext.config")
+-- require("vim._extui").enable({ msg = { target = "msg" } })
+-- vim.opt.cmdheight = 0
--- lua/plugins/neotree.lua
+++
@@ -1,9 +0,0 @@
- vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
- vim.fn.sign_define("DiagnosticSignError",
- {text = " ", texthl = "DiagnosticSignError"})
- vim.fn.sign_define("DiagnosticSignWarn",
- {text = " ", texthl = "DiagnosticSignWarn"})
- vim.fn.sign_define("DiagnosticSignInfo",
- {text = " ", texthl = "DiagnosticSignInfo"})
- vim.fn.sign_define("DiagnosticSignHint",
- {text = "󰌵", texthl = "DiagnosticSignHint"})
