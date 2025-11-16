8b99e6abb4f02809af25d86e8c9f0c9634c27ae2
Mihail <zoood3.14@mail.com>
2025-11-16 22:01:26 +0300

    DONE: insert mason and terminal

--- lua/vnext/plugins/file-manager.lua
+++ lua/vnext/plugins/file-manager.lua
@@ -1,6 +1,6 @@
 return {
   {
--- Файловый менеджер (проводник) запуск на "-"
+    -- Файловый менеджер (проводник) запуск на "-"
     "A7Lavinraj/fyler.nvim",
     dependencies = { "nvim-mini/mini.icons" },
     branch = "stable",
@@ -9,16 +9,16 @@ return {
     },
     opts = {},
   },
-
   {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
     cmd = "Neotree",
     keys = {
       { "\\", "<cmd>Neotree reveal toggle<cr>", desc = "Toggle Filetree" },
+      { "<leader>fg", "<cmd>Neotree float grep<cr>", desc = "Grep (float)" },
+      { "<leader>ft", "<cmd>Neotree float terminal<cr>", desc = "Terminal (float)" },
     },
     init = function()
-      -- Load neo-tree when nvim is started with a directory
       vim.api.nvim_create_autocmd("BufEnter", {
         group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
         desc = "Start Neo-tree with directory",
@@ -36,7 +36,7 @@ return {
       })
     end,
     opts = {
-      close_if_last_window = true, -- Close Neo-tree if it is the last window left in the tab
+      close_if_last_window = true,
       default_component_configs = {
         indent = {
           with_markers = false,
@@ -47,7 +47,6 @@ return {
       },
       filesystem = {
         commands = {
-          -- Override delete to use trash instead of rm
           delete = function(state)
             local path = state.tree:get_node().path
             vim.fn.system({ "trash", vim.fn.fnameescape(path) })
@@ -56,10 +55,8 @@ return {
           system_open = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
-            -- MacOS
             if vim.fn.executable("open") == 1 then
               vim.api.nvim_command("silent !open -g " .. path)
-            -- Linux
             elseif vim.fn.executable("xdg-open") == 1 then
               vim.api.nvim_command("silent !xdg-open " .. path)
             else
@@ -73,7 +70,6 @@ return {
             ["H"] = "toggle_hidden",
             ["/"] = "fuzzy_finder",
             ["D"] = "fuzzy_finder_directory",
-            --["/"] = "filter_as_you_type", -- this was the default until v1.28
             ["f"] = "filter_on_submit",
             ["<C-x>"] = "clear_filter",
             ["<bs>"] = "navigate_up",
@@ -82,45 +78,78 @@ return {
             ["]g"] = "next_git_modified",
             ["<space>"] = {
               "toggle_node",
-              nowait = false, -- disable `nowait` if you have existing combos starting with this char that you want to use
+              nowait = false,
             },
             ["<2-LeftMouse>"] = "open",
             ["<cr>"] = "open",
             ["s"] = "open_split",
             ["v"] = "open_vsplit",
             ["t"] = "open_tabnew",
-            --["P"] = "toggle_preview",
             ["C"] = "close_node",
             ["z"] = "close_all_nodes",
-            --["Z"] = "expand_all_nodes",
             ["R"] = "refresh",
             ["a"] = {
               "add",
-              -- some commands may take optional config options, see `:h neo-tree-mappings` for details
               config = {
-                show_path = "none", -- "none", "relative", "absolute"
+                show_path = "none",
               },
             },
-            ["A"] = "add_directory", -- also accepts the config.show_path option.
-            ["d"] = "noop", -- unbind delete
-            ["dd"] = "delete", -- bind delete to new mapping
+            ["A"] = "add_directory",
+            ["d"] = "noop",
+            ["dd"] = "delete",
             ["r"] = "rename",
             ["y"] = "copy_to_clipboard",
             ["x"] = "cut_to_clipboard",
             ["p"] = "paste_from_clipboard",
-            ["c"] = "copy", -- takes text input for destination, also accepts the config.show_path option
-            ["m"] = "move", -- takes text input for destination, also accepts the config.show_path option
+            ["c"] = "copy",
+            ["m"] = "move",
             ["q"] = "close_window",
             ["?"] = "show_help",
             ["<"] = "prev_source",
             [">"] = "next_source",
           },
         },
-        hijack_netrw_behavior = "open_current", -- netrw disabled, opening a directory opens within the window like netrw would, regardless of window.position
+        hijack_netrw_behavior = "open_current",
+      },
+      -- Настройка для плавающих окон
+      window = {
+        position = "left",
+        width = 30,
+        mappings = {
+          ["g"] = "noop",
+          ["G"] = "noop",
+        },
+      },
+      default_component_configs = {
+        git_status = {
+          symbols = {
+            added     = "",
+            modified  = "",
+            deleted   = "",
+            renamed   = "",
+            untracked = "",
+            ignored   = "",
+            unstaged  = "",
+            staged    = "",
+            conflict  = "",
+          },
+        },
+      },
+      -- Настройка для плавающих окон grep и терминала
+      float = {
+        enable = true,
+        open_win_config = {
+          relative = "editor",
+          border = "rounded",
+          width = function() return math.floor(vim.o.columns * 0.8) end,
+          height = function() return math.floor(vim.o.lines * 0.8) end,
+          row = function() return math.floor((vim.o.lines - vim.o.lines * 0.8) / 2) end,
+          col = function() return math.floor((vim.o.columns - vim.o.columns * 0.8) / 2) end,
+          style = "minimal",
+        },
       },
     },
     config = function(_, opts)
-      -- no background for Neotree
       vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "NONE" })
       vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "NONE" })
       require("neo-tree").setup(opts)
@@ -135,3 +164,4 @@ return {
     end,
   },
 }
+
